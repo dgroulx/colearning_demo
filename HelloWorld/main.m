4a5ebacb9bc2a73adb9299fd2bdca4c5a364ff87
David Groulx <david@sandbendersoftware.com>
2014-04-24 18:10:32 -0400

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by David Groulx on 4/24/14.
+//  Copyright (c) 2014 David Groulx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
